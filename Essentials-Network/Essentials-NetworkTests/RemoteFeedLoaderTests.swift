933ab504a74275ec93002005aa7c1ad57554716e
Ivo Dutra <ivogdutra@gmail.com>
2022-11-19 12:01:15 -0300

    Refactor code to avoid duplication

--- Essentials-Network/Essentials-NetworkTests/RemoteFeedLoaderTests.swift
+++ Essentials-Network/Essentials-NetworkTests/RemoteFeedLoaderTests.swift
@@ -27,35 +27,46 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: anyURL(), client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: anyURL(), client: client)
+        let url = URL(string: "a-different-url.com")!
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
-        XCTAssertEqual(client.requestedURL, anyURL())
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
 
 // MARK: - Helpers
 private extension RemoteFeedLoaderTests {
+    
+    // MARK: - Spy
+    
+    class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+    
+    // MARK: - Factories
+    
+    func makeSUT(url: URL = URL(string: "a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    // TODO: why you can't use anyURL as default paramether?
     func anyURL() -> URL {
         URL(string: "a-url.com")!
     }
